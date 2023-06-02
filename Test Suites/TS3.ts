2881fbc7615526606c9a64bfd66216821cc1250e
Thalluru Saiteja <thalluru.s@mantralabsglobal.com>
2023-06-02 09:23:11 +0530

    New Test Suite

---
+++ Test Suites/TS3.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2f5f3a01-0cf6-4bd4-9ed7-55e803b51eeb</testSuiteGuid>
+</TestSuiteEntity>
